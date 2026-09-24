2d8976d45d310203ccb5d888c287be7468856087
tungtunggod1 <lucienlin936@gmail.com>
2026-09-24 10:07:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,3 +41,11 @@ print(f"Your number is {number(x)}.") """
 x = str(input("How was your service. "))
 print(f"I will tip {service(x)}.") """
 
+""" def x(number):
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+number = int(input("Pick a number. "))
+print(f"The number's factors are{x(number)}.") """
